90b8121cf29637b469ca78b812ec8e5db8eea1a6
Unknown <paula.boules@pharos-solutions.de>
2019-06-14 16:58:48 +0200

    movies cell design

---
+++ MoviesApp/Scenes/MoviesList/MovieTableViewCell/MovieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTableViewCell.swift
+//  MoviesApp
+//
+//  Created by Paula Boules on 6/14/19.
+//  Copyright © 2019 Paula Boules. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
