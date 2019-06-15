a734b87ff79560c52222319d6e45e1c9e7a11977
PBBassily <paula.boules@pharos-solutions.de>
2019-06-15 14:26:04 +0200

    stageful view controller

--- MoviesApp/Scenes/MovieCreation/MovieCreationAndDetails.swift
+++ MoviesApp/Scenes/MovieCreation/MovieCreationAndDetails.swift
@@ -9,5 +9,29 @@
 import UIKit
 
 internal class MovieCreationAndDetails: UIViewController {
+    internal var usageType: UsageType = .movieCreation
+    
+    override internal func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override internal func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if usageType == .movieCreation {
+            navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(save))
+        }
+        title = usageType.rawValue
+    }
+    
+    @objc private func save() {
+        
+    }
+    
+}
 
+extension MovieCreationAndDetails {
+    internal enum UsageType: String {
+        case showingMovieDetails = "Movie details"
+        case movieCreation = "Create your movie"
+    }
 }
