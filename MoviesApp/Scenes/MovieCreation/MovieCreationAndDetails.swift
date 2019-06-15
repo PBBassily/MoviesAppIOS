4e56b5b326eb9863b87db93dd2c56e36fbb1e4bf
PBBassily <paula.boules@pharos-solutions.de>
2019-06-15 14:17:57 +0200

    navigation

---
+++ MoviesApp/Scenes/MovieCreation/MovieCreationAndDetails.swift
@@ -0,0 +1,13 @@
+//
+//  MovieCreationAndDetails.swift
+//  MoviesApp
+//
+//  Created by Paula Boules on 6/15/19.
+//  Copyright © 2019 Paula Boules. All rights reserved.
+//
+
+import UIKit
+
+internal class MovieCreationAndDetails: UIViewController {
+
+}
